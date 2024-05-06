89aa34b68945b7e5924b7b6e4d965fd63194fc77
Mridhoalamsyah <161903982+Mridhoalamsyah@users.noreply.github.com>
2024-05-06 17:15:48 +0700

    Add files via upload

---
+++ database/migrations/2024_05_06_101602_create_guru2s_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guru2s', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_guru');
+            $table->boolean('jenis_kelamin');
+            $table->text('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guru2s');
+    }
+};
